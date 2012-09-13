dee72790385070238d06488c9a9be876cff027e1
R.J. Lavergne <rjlavergne@Roberts-MacBook-Pro.local>
2012-09-13 15:39:18 -0400

    Added Tab for Contact Info

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-/* Author:
 
-*/
-
+$('#reference').on('hover', function(event) {
+	$('#contact-me').fadeIn(500);
+	$('#contact-me').on({
+		mouseeneter: function(event) {
+			$('#contact-me').css('display','block');
+		},
+		mouseleave: function(event) {
+			$('#contact-me').fadeOut(300);
+		}
+	});
+	$('#contact-me').fadeOut(300);
+});
 
 
 
